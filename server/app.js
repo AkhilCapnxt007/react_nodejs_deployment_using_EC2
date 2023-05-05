49d39f0c2ccfeba908681258693b3ff509b0dea8
Akhil Nayak <aakhiljay06@gmail.com>
2023-05-05 15:58:06 +0530

    changed prod

--- server/app.js
+++ server/app.js
@@ -29,19 +29,24 @@ console.log(
 );
 
 // NodeENV AKADA
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/client/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "client", "build", "index.html")
+//     );
+//   });
+// } else {
+// app.get("/", (req, res) => {
   app.use(express.static(path.join(__dirname, "/client/build")));
-
   app.get("*", (req, res) => {
     res.sendFile(
       path.resolve(__dirname, "../", "client", "build", "index.html")
     );
   });
-} else {
-  app.get("/", (req, res) => {
-    res.send("Server is Running! 🚀");
-  });
-}
+  // res.send("Server is Running! 🚀");
+// });
+// }
 
 //error handling
 app.use((err, req, res, next) => {

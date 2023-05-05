6149ee1287387e4463fae6a4008b6922cd40f85a
Akhil Nayak <aakhiljay06@gmail.com>
2023-05-05 15:31:28 +0530

    port number changes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const PORT = 4000;
 const app = express();
 const path = require("path");
 const dotenv = require("dotenv");
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const app = require("./app.js");
 
 
-var port = 4000 || process.env.PORT;
+var port = 80 || process.env.PORT;
 
 var server = app.listen(port , function(){
     console.log('server listening at port ' + port);
